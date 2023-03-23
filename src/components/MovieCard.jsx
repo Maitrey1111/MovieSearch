16a4709c027f59825ec3382a7c7fe2504a1bec2f
Maitrey Bhute <maitreybhute2806@gmail.com>
2023-03-23 06:38:54 +0530

    final commit

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,8 +1,30 @@
-import {React} from "react";
+import { React, useEffect, useState } from "react";
+import axios from "axios";
 import "../static/MovieCard.css";
 
 function MovieCard({posterPath, title, date, description, rating, movieId}) {
   const url = "https://www.themoviedb.org/t/p/w220_and_h330_face/" + posterPath;
+  const video_url = "https://api.themoviedb.org/3/movie/"+movieId+"/videos?api_key=98ae3afcd7b18c25ccd2d93f00f88c42&language=en-US"
+
+  const [link, setLink] = useState()
+
+  useEffect(() => {
+    axios
+      .get(
+        video_url
+      )
+      .then((resp) => {
+        const data = resp.data.results;
+        if(data.length > 0){
+          const link = 'https://www.youtube.com/watch?v=' + data[0].key;
+          setLink(link);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [movieId]);
+
   return (
     <a href="/" rel="noreferrer" className="holder">
       <div className="poster">
@@ -14,7 +36,7 @@ function MovieCard({posterPath, title, date, description, rating, movieId}) {
         <h5>{rating.toFixed(1)} out of 5 </h5>
         <p>{description}</p>
         <span>...</span>
-        <button onClick={()=>window.location.href='https://google.com'}>Watch Now</button>
+        <a href={link} target={"_blank"} rel="noreferrer">Glance</a>
       </div>
     </a>
   );
